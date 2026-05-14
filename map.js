792b74daea58b8e3a53359504981c889200b5366
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 09:52:15 -0700

    added green bike routes

--- map.js
+++ map.js
@@ -15,3 +15,21 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+map.on('load', async () => {
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+});
