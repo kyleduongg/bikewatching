00b97a93f6a2600b51f0c8fa282f389caea6a980
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 10:33:25 -0700

    calculating traffic + size markers

--- map.js
+++ map.js
@@ -73,7 +73,7 @@ map.on('load', async () => {
 
     console.log('Loaded JSON Data:', jsonData);
 
-    const stations = jsonData.data.stations;
+    let stations = jsonData.data.stations;
 
     console.log('Stations Array:', stations);
 
@@ -81,13 +81,42 @@ map.on('load', async () => {
 
     console.log('Loaded Trips:', trips);
 
+    const departures = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.start_station_id,
+    );
+
+    const arrivals = d3.rollup(
+      trips,
+      (v) => v.length,
+      (d) => d.end_station_id,
+    );
+
+    stations = stations.map((station) => {
+      let id = station.short_name;
+
+      station.arrivals = arrivals.get(id) ?? 0;
+      station.departures = departures.get(id) ?? 0;
+      station.totalTraffic = station.arrivals + station.departures;
+
+      return station;
+    });
+
+    console.log('Stations with traffic:', stations);
+
+    const radiusScale = d3
+      .scaleSqrt()
+      .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+      .range([0, 25]);
+
     // Append circles to the SVG for each station
     const circles = svg
       .selectAll('circle')
       .data(stations)
       .enter()
       .append('circle')
-      .attr('r', 5)
+      .attr('r', (d) => radiusScale(d.totalTraffic))
       .attr('fill', 'steelblue')
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
