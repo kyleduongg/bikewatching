79c11c43b74f324d1db5ac652d1e66a937aec807
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 10:41:09 -0700

    tooltip

--- map.js
+++ map.js
@@ -120,7 +120,14 @@ map.on('load', async () => {
       .attr('fill', 'steelblue')
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
-      .attr('opacity', 0.8);
+      .attr('opacity', 0.8)
+      .each(function (d) {
+        d3.select(this)
+          .append('title')
+          .text(
+            `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+          );
+      });
 
     // Function to update circle positions when the map moves/zooms
     function updatePositions() {
