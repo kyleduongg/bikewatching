13a8d660be45272df387b2521d05f172b301785f
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 11:16:56 -0700

    reactivity done

--- map.js
+++ map.js
@@ -18,6 +18,48 @@ function formatTime(minutes) {
   return date.toLocaleString('en-US', { timeStyle: 'short' });
 }
 
+function computeStationTraffic(stations, trips) {
+  const departures = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.start_station_id,
+  );
+
+  const arrivals = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.end_station_id,
+  );
+
+  return stations.map((station) => {
+    let id = station.short_name;
+
+    station.arrivals = arrivals.get(id) ?? 0;
+    station.departures = departures.get(id) ?? 0;
+    station.totalTraffic = station.arrivals + station.departures;
+
+    return station;
+  });
+}
+
+function minutesSinceMidnight(date) {
+  return date.getHours() * 60 + date.getMinutes();
+}
+
+function filterTripsbyTime(trips, timeFilter) {
+  return timeFilter === -1
+    ? trips
+    : trips.filter((trip) => {
+        const startedMinutes = minutesSinceMidnight(trip.started_at);
+        const endedMinutes = minutesSinceMidnight(trip.ended_at);
+
+        return (
+          Math.abs(startedMinutes - timeFilter) <= 60 ||
+          Math.abs(endedMinutes - timeFilter) <= 60
+        );
+      });
+}
+
 // Set your Mapbox access token here
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3lkdW9uZzEiLCJhIjoiY21wNXBleW8zMTJheDJ5bzQwZ3JiMXJsaCJ9.1Z_6HloVPtaR1689yQjpMg';
 
@@ -84,31 +126,15 @@ map.on('load', async () => {
 
     console.log('Stations Array:', stations);
 
-    const trips = await d3.csv(BLUEBIKES_TRAFFIC_URL);
+    let trips = await d3.csv(BLUEBIKES_TRAFFIC_URL, (trip) => {
+      trip.started_at = new Date(trip.started_at);
+      trip.ended_at = new Date(trip.ended_at);
+      return trip;
+    });
 
     console.log('Loaded Trips:', trips);
 
-    const departures = d3.rollup(
-      trips,
-      (v) => v.length,
-      (d) => d.start_station_id,
-    );
-
-    const arrivals = d3.rollup(
-      trips,
-      (v) => v.length,
-      (d) => d.end_station_id,
-    );
-
-    stations = stations.map((station) => {
-      let id = station.short_name;
-
-      station.arrivals = arrivals.get(id) ?? 0;
-      station.departures = departures.get(id) ?? 0;
-      station.totalTraffic = station.arrivals + station.departures;
-
-      return station;
-    });
+    stations = computeStationTraffic(stations, trips);
 
     console.log('Stations with traffic:', stations);
 
@@ -120,7 +146,7 @@ map.on('load', async () => {
     // Append circles to the SVG for each station
     const circles = svg
       .selectAll('circle')
-      .data(stations)
+      .data(stations, (d) => d.short_name)
       .enter()
       .append('circle')
       .attr('r', (d) => radiusScale(d.totalTraffic))
@@ -161,13 +187,33 @@ map.on('load', async () => {
 
       if (timeFilter === -1) {
         selectedTime.textContent = '';
-        anyTimeLabel.style.display = 'inline-block';
+        anyTimeLabel.style.display = 'block';
       } else {
         selectedTime.textContent = formatTime(timeFilter);
         anyTimeLabel.style.display = 'none';
       }
 
-      // Filtering logic comes in the next step
+      updateScatterPlot(timeFilter);
+    }
+
+    function updateScatterPlot(timeFilter) {
+      const filteredTrips = filterTripsbyTime(trips, timeFilter);
+
+      const filteredStations = computeStationTraffic(stations, filteredTrips);
+
+      timeFilter === -1
+        ? radiusScale.range([0, 25])
+        : radiusScale.range([3, 50]);
+
+      circles
+        .data(filteredStations, (d) => d.short_name)
+        .join('circle')
+        .attr('r', (d) => radiusScale(d.totalTraffic))
+        .select('title')
+        .text(
+          (d) =>
+            `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+        );
     }
 
     timeSlider.addEventListener('input', updateTimeDisplay);
