d4c55c21b9463179deb5052dffb0cc82a48ccf9c
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 11:27:34 -0700

    legend and circle colors!!!

--- map.js
+++ map.js
@@ -13,6 +13,8 @@ const BLUEBIKES_TRAFFIC_URL =
 
 let timeFilter = -1;
 
+let stationFlow = d3.scaleQuantize().domain([0, 1]).range([0, 0.5, 1]);
+
 function formatTime(minutes) {
   const date = new Date(0, 0, 0, 0, minutes);
   return date.toLocaleString('en-US', { timeStyle: 'short' });
@@ -150,10 +152,12 @@ map.on('load', async () => {
       .enter()
       .append('circle')
       .attr('r', (d) => radiusScale(d.totalTraffic))
-      .attr('fill', 'steelblue')
       .attr('stroke', 'white')
       .attr('stroke-width', 1)
       .attr('opacity', 0.8)
+      .style('--departure-ratio', (d) =>
+        stationFlow(d.totalTraffic === 0 ? 0.5 : d.departures / d.totalTraffic),
+      )
       .each(function (d) {
         d3.select(this)
           .append('title')
@@ -209,6 +213,9 @@ map.on('load', async () => {
         .data(filteredStations, (d) => d.short_name)
         .join('circle')
         .attr('r', (d) => radiusScale(d.totalTraffic))
+        .style('--departure-ratio', (d) =>
+          stationFlow(d.totalTraffic === 0 ? 0.5 : d.departures / d.totalTraffic),
+        )
         .select('title')
         .text(
           (d) =>
