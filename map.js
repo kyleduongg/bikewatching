467d133165d0c1480f57b9e5937ce94701115891
Kyle Duong <kyduong@ucsd.edu>
2026-05-14 09:42:06 -0700

    create th emap

--- map.js
+++ map.js
@@ -2,3 +2,16 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3lkdW9uZzEiLCJhIjoiY21wNXBleW8zMTJheDJ5bzQwZ3JiMXJsaCJ9.1Z_6HloVPtaR1689yQjpMg';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
